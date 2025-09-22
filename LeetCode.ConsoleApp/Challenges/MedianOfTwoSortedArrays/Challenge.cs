be71fabfee9793be1f4a10bb0b4825c9c800b4ea
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-09-22 16:27:59 -0300

    refactor: altera o desafio MedianOfTwoSortedArrays para usar o MySolution2 e usar vários inputs de teste

--- LeetCode.ConsoleApp/Challenges/MedianOfTwoSortedArrays/Challenge.cs
+++ LeetCode.ConsoleApp/Challenges/MedianOfTwoSortedArrays/Challenge.cs
@@ -10,15 +10,31 @@ namespace LeetCode.ConsoleApp.Challenges.MedianOfTwoSortedArrays
 
         public Challenge()
         {
-            Input1 = new int[] { 1, 3,2 };
-            Input2 = new int[] { 2 };
+            //// Exemplo 2
+            Input1 = new int[] { 1, 2 };
+            Input2 = new int[] { 3, 4 };
+            //// Output esperado: 2.5   (pois a combinação é [1,2,3,4])
         }
 
         [Benchmark]
         public void RunMy()
         {
-            var solution = new MySolution(Input1, Input2);
-            solution.Run();
+            var inputs = new List<(int[], int[])>()
+            {
+                (new int[] { 1, 3 }, new int[] { 2 }),                   // Exemplo 1 → Output esperado: 2.0
+                (new int[] { 1, 2 }, new int[] { 3, 4 }),                // Exemplo 2 → Output esperado: 2.5
+                (new int[] { 0, 0 }, new int[] { 0, 0 }),                // Exemplo 3 → Output esperado: 0.0
+                (new int[] { }, new int[] { 1 }),                        // Exemplo 4 → Output esperado: 1.0
+                (new int[] { 2 }, new int[] { }),                        // Exemplo 5 → Output esperado: 2.0
+                (new int[] { 1, 2, 5, 7 }, new int[] { 3, 4, 6, 8, 9 }), // Exemplo 6 → Output esperado: 5.0
+                (new int[] { -5, -3, -1, 0 }, new int[] { 1, 2, 3, 4, 5 }) // Exemplo 7 → Output esperado: 1.0
+            };
+
+            foreach (var (input1, input2) in inputs)
+            {
+                var solution = new MySolution2(input1, input2);
+                solution.Run();
+            }
         }
 
         [Benchmark]
