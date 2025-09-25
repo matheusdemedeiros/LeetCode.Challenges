1f533f90b614448f5451bd01ca9847a5398c0ea2
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2025-09-25 15:07:37 -0300

    feat: detalha a BestSolution do desafio ContainerWithMostWater

--- LeetCode.ConsoleApp/Challenges/ContainerWithMostWater/BestSolution.cs
+++ LeetCode.ConsoleApp/Challenges/ContainerWithMostWater/BestSolution.cs
@@ -1,9 +1,12 @@
-﻿namespace LeetCode.ConsoleApp.Challenges.ContainerWithMostWater
+﻿using System.ComponentModel;
+
+namespace LeetCode.ConsoleApp.Challenges.ContainerWithMostWater
 {
     public class BestSolution
     {
         public void Run()
         {
+            //Neste caso a solução ótima é idêntica à minha.
         }
     }
 }
